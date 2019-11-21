29b2f91ab29c7d42ac8dc3188f84a99ed0082676
youyuyixiu <xiaoshuyi123@163.com>
2019-11-21 18:58:48 +0800

    day03--day

--- test.py
+++ test.py
@@ -230,4 +230,45 @@ day04--循环结构
 # else:
 #     print('%d不是素数' % num)
 
-# ex2:求两个正整数的最大公约数和最小公倍数
+# # ex2:求两个正整数的最大公约数和最小公倍数
+# x = int(input('x = '))
+# y = int(input('y = '))
+# if x > y:
+#     x, y = y, x
+# for factor in range(x, 0, -1):
+#     if x % factor == 0 and y % factor == 0:
+#         print('%d和%d的最大公约数是%d' % (x, y, factor))
+#         print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
+#         break
+
+# row = int(input('请输入行数：'))
+# for i in range(row):
+#     for _ in range(i+1):
+#         print('*', end='')
+#     print()
+
+# row = int(input('请输入行数：'))
+# for i in range(row):
+#     for j in range(row):
+#         if j < row - i - 1:
+#             print(' ', end='')
+#         else:
+#             print('*', end='')
+#     print()
+
+# row = int(input('请输入行数：'))
+# for i in range(row):
+#     for j in range(row):
+#         if j < row - i - 1:
+#             print(' ', end='')
+#         else:
+#             print('*', end='')
+#     print()
+
+row = int(input('请输入行数：'))
+for i in range(row):
+    for _ in range(row - i -1):
+        print(' ',end='')
+    for _ in range(2 * i + 1):
+        print('*',end='')
+    print()
