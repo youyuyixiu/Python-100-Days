27baab918efbba5b15825710089b778998d1d4b8
youyuyixiu <xiaoshuyi123@163.com>
2019-11-22 08:37:46 +0800

    day04--day

--- test.py
+++ test.py
@@ -265,10 +265,10 @@ day04--循环结构
 #             print('*', end='')
 #     print()
 
-row = int(input('请输入行数：'))
-for i in range(row):
-    for _ in range(row - i -1):
-        print(' ',end='')
-    for _ in range(2 * i + 1):
-        print('*',end='')
-    print()
+# row = int(input('请输入行数：'))
+# for i in range(row):
+#     for _ in range(row - i -1):
+#         print(' ',end='')
+#     for _ in range(2 * i + 1):
+#         print('*',end='')
+#     print()
