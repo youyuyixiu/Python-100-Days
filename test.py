4a7c1a8fc8a33faaec9601033b2a11c49e6a7c81
youyuyixiu <xiaoshuyi123@163.com>
2019-11-25 23:43:12 +0800

    day08-night

--- test.py
+++ test.py
@@ -1001,3 +1001,109 @@ import sys
 """
 day08--面向对象编程基础
 """
+# # class
+# class Student(object):
+#     # __init__是一个特殊方法用于在创建对象时进行初始化操作
+#     # 通过这个方法我们可以为学生对象绑定name和age两个属性
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+    
+#     def study(self, course_name):
+#         print('%s正在学习%s.' % (self.name, course_name))
+    
+#     def watch_movie(self):
+#         if self.age < 18:
+#             print('%s只能观看《熊出没》.' % self.name)
+#         else:
+#             print('%s正在观看岛国爱情大电影.' % self.name)
+
+# def main():
+#     stu1 = Student('xsy', 18)
+#     stu1.study('Python程序设计')
+#     stu1.watch_movie()
+
+# if __name__ == '__main__':
+#     main()
+
+# class Test:
+#     def __init__(self, foo):
+#         self.__foo = foo
+#     def __bar(self):
+#         print(self.__foo)
+#         print('__bar')
+
+# def main():
+#     test = Test('hello')
+#     test.__bar()
+#     print(test.__foo)
+
+# if __name__ == '__main__':
+#     main()
+
+# # ex1:定义一个类描述数字时钟
+# from time import sleep
+# class Clock(object):
+#     def __init__(self, hour=0, minute=0, second=0):
+#         self.hour = hour
+#         self.minute = minute
+#         self.second = second
+    
+#     def run(self):
+#         self.second += 1
+#         if self.second == 60:
+#             self.second = 0
+#             self.minute += 1
+#             if self.minute == 60:
+#                 self.minute = 0
+#                 self.hour += 1
+#                 if self.hour == 24:
+#                     self.hour = 0
+    
+#     def show(self):
+#         return '%02d:%02d:%02d' % (self.hour, self.minute, self.second)
+
+# def main():
+#     clock = Clock(23, 59, 58)
+#     while True:
+#         print(clock.show())
+#         sleep(1)
+#         clock.run()
+
+# if __name__ == '__main__':
+#     main()
+
+# # ex2:定义一个类描述平面上的点并提供移动点和计算到另一个点距离的方法
+# from math import sqrt
+# class Point(object):
+#     def __init__(self, x=0, y=0):
+#         self.x = x
+#         self.y = y
+    
+#     def move_to(self, x, y):
+#         self.x = x
+#         self.y = y
+    
+#     def move_by(self, dx, dy):
+#         self.x += dx
+#         self.y += dy
+    
+#     def distance_to(self, other):
+#         dx = self.x - other.x
+#         dy = self.y - other.y
+#         return sqrt(dx ** 2 + dy ** 2)
+    
+#     def __str__(self):
+#         return '(%s, %s)' % (str(self.x), str(self.y))
+
+# def main():
+#     p1 = Point(3, 5)
+#     p2 = Point()
+#     print(p1)
+#     print(p2)
+#     p2.move_by(-1, 2)
+#     print(p2)
+#     print(p1.distance_to(p2))
+
+# if __name__ == '__main__':
+#     main()
