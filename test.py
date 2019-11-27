da980d526557dc6f60e05f2aa3c2619408feb06d
youyuyixiu <xiaoshuyi123@163.com>
2019-11-27 23:45:52 +0800

    day09-night

--- test.py
+++ test.py
@@ -1107,3 +1107,162 @@ day08--面向对象编程基础
 
 # if __name__ == '__main__':
 #     main()
+
+
+"""
+day09--面向对象进阶
+"""
+# @property包装器
+# class Person(object):
+    
+#     def __init__(self, name, age):
+#         self._name = name
+#         self._age = age
+    
+#     # 访问器 - getter方法
+#     @property
+#     def name(self):
+#         return self._name
+
+#     # 访问器 - getter方法
+#     @property
+#     def age(self):
+#         return self._age
+    
+#     # 修改器 - setter方法
+#     @age.setter
+#     def age(self, age):
+#         self._age = age
+    
+#     def play(self):
+#         if self._age <= 16:
+#             print('%s正在玩飞行棋.' % self._name)
+#         else:
+#             print('%s正在玩斗地主.' % self._name)
+
+# def main():
+#     person = Person('王大锤', 12)
+#     person.play()
+#     person.age = 22
+#     person.play()
+
+# if __name__ == '__main__':
+#     main()
+
+# # __slot__
+# class Person(object):
+
+#     __slot__ = ('_name', '_age', '_gender')
+    
+#     def __init__(self, name, age):
+#         self._name = name
+#         self._age = age
+    
+#     # 访问器 - getter方法
+#     @property
+#     def name(self):
+#         return self._name
+
+#     # 访问器 - getter方法
+#     @property
+#     def age(self):
+#         return self._age
+    
+#     # 修改器 - setter方法
+#     @age.setter
+#     def age(self, age):
+#         self._age = age
+    
+#     def play(self):
+#         if self._age <= 16:
+#             print('%s正在玩飞行棋.' % self._name)
+#         else:
+#             print('%s正在玩斗地主.' % self._name)
+
+# def main():
+#     person = Person('王大锤', 22)
+#     person.play()
+#     person._gender = '男'
+#     # person.play()
+
+# if __name__ == '__main__':
+#     main()
+
+# from math import sqrt
+
+# class Triangle(object):
+#     def __init__(self, a, b, c):
+#         self._a = a
+#         self._b = b
+#         self._c = c
+
+#     @staticmethod
+#     def is_valid(a, b, c):
+#         return a + b > c and a + c > b and b + c > a
+
+#     def perimeter(self):
+#         return self._a + self._b + self._c
+
+#     def area(self):
+#         half = self.perimeter() / 2
+#         return sqrt(half * (half - self._a) * (half - self._b) * (half - self._c))
+
+# def main():
+#     a, b, c = 3, 4, 5
+#     if Triangle.is_valid(a, b, c):
+#         t = Triangle(a, b, c)
+#         print(t.perimeter())
+#         # print(Triangle.perimeter(t))
+#         print(t.area())
+#         # print(Triangle.area())
+#     else:
+#         print('无法构成三角形')
+
+# if __name__ == '__main__':
+#     main()
+
+# from time import time, localtime, sleep
+
+# class Clock(object):
+#     """数字时钟"""
+
+#     def __init__(self, hour=0, minute=0, second=0):
+#         self._hour = hour
+#         self._minute = minute
+#         self._second = second
+
+#     @classmethod
+#     def now(cls):
+#         ctime = localtime(time())
+#         return cls(ctime.tm_hour, ctime.tm_min, ctime.tm_sec)
+
+#     def run(self):
+#         """走字"""
+#         self._second += 1
+#         if self._second == 60:
+#             self._second = 0
+#             self._minute += 1
+#             if self._minute == 60:
+#                 self._minute = 0
+#                 self._hour += 1
+#                 if self._hour == 24:
+#                     self._hour = 0
+
+#     def show(self):
+#         """显示时间"""
+#         return '%02d:%02d:%02d' % \
+#                (self._hour, self._minute, self._second)
+
+
+# def main():
+#     # 通过类方法创建对象并获取系统时间
+#     clock = Clock.now()
+#     while True:
+#         print(clock.show())
+#         sleep(1)
+#         clock.run()
+
+
+# if __name__ == '__main__':
+#     main()
+
