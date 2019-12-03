dbbbd28aad0809e082f35400feb7d3eabd842e45
youyuyixiu <xiaoshuyi123@163.com>
2019-12-03 23:12:17 +0800

    day09-night

--- test.py
+++ test.py
@@ -1111,3 +1111,125 @@ day08--面向对象编程基础
 """
 day09--面向对象进阶
 """
+# # __slot__
+# class Person(object):
+
+#     # 限定Person对象只能绑定_name, _age和_gender属性
+#     __slots__ = ('_name', '_age', '_gender')
+
+#     def __init__(self, name, age):
+#         self._name = name
+#         self._age = age
+    
+#     @property
+#     def name(self):
+#         return self._name
+    
+#     @property
+#     def age(self):
+#         return self._age
+    
+#     @age.setter
+#     def age(self, age):
+#         self._age = age
+
+#     def play(self):
+#         if self._age <= 16:
+#             print('%s正在玩飞行棋.' % self._name)
+#         else:
+#             print('%s正在玩斗地主.' % self._name)
+
+# def main():
+#     person = Person('肖', 27)
+#     person.play()
+#     person.age = 15
+#     person.play()
+#     person._gender = '男'
+#     print(person._gender)
+#     # person._sex = '女'
+
+# if __name__ == "__main__":
+#     main()
+
+# # 静态方法
+# from math import sqrt
+# class Triangle(object):
+
+#     def __init__(self, a, b, c):
+#         self._a = a
+#         self._b = b
+#         self._c = c
+
+#     @staticmethod
+#     def is_valid(a, b, c):
+#         return a + b > c and a + c > b and b + c > a
+    
+#     def perimeter(self):
+#         return self._a + self._b + self._c
+    
+#     def area(self):
+#         half = self.perimeter() / 2
+#         return sqrt(half * (half - self._a) * (half - self._b) * (half - self._c))
+
+# def main():
+#     a, b, c = 3, 4, 5
+#     if Triangle.is_valid(a, b, c):
+#         t = Triangle(a, b, c)
+#         print(t.perimeter())
+#         print(Triangle.perimeter(t))
+#         print(t.area())
+#         print(Triangle.area(t))
+#     else:
+#         print('无法构成三角形')
+
+# if __name__ == '__main__':
+#     main()
+
+# 类方法
+from time import time, localtime, sleep
+
+
+class Clock(object):
+    """数字时钟"""
+
+    def __init__(self, hour=0, minute=0, second=0):
+        self._hour = hour
+        self._minute = minute
+        self._second = second
+
+    @classmethod
+    def now(cls):
+        ctime = localtime(time())
+        return cls(ctime.tm_hour, ctime.tm_min, ctime.tm_sec)
+
+    def run(self):
+        """走字"""
+        self._second += 1
+        if self._second == 60:
+            self._second = 0
+            self._minute += 1
+            if self._minute == 60:
+                self._minute = 0
+                self._hour += 1
+                if self._hour == 24:
+                    self._hour = 0
+
+    def show(self):
+        """显示时间"""
+        return '%02d:%02d:%02d' % \
+               (self._hour, self._minute, self._second)
+
+
+def main():
+    # 通过类方法创建对象并获取系统时间
+    clock = Clock.now()
+    while True:
+        print(clock.show())
+        sleep(1)
+        clock.run()
+
+if __name__ == '__main__':
+    main()
+
+# 类之间的关系
+
