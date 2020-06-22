f774fdfb712354b733e30cc4ce697d21fdb4ff7c
youyuyixiu <xiaoshuyi123@163.com>
2020-06-22 22:34:33 +0800

    day05-2

--- Day01-15/code/Day05/test.py
+++ Day01-15/code/Day05/test.py
@@ -21,48 +21,84 @@
 """
 百鸡百钱
 """
-for x in range(0,20):
-    for y in range(0,33):
-        z = 100 - x - y
-        if 5 * x + 3 * y + z / 3 == 100:
-            print('%d只公鸡, %d只母鸡，%d只小鸡' %(x, y, z))
+# for x in range(0,20):
+#     for y in range(0,33):
+#         z = 100 - x - y
+#         if 5 * x + 3 * y + z / 3 == 100:
+#             print('%d只公鸡, %d只母鸡，%d只小鸡' %(x, y, z))
 
 """
 Craps赌博游戏
 我们设定玩家开始游戏时有1000元的赌注
 游戏结束的条件是玩家输光所有的赌注
 """
-from random import randint
+# from random import randint
 
-money = 1000
-while money > 0:
-    print('你的总资产是：',money)
-    needs_go_on = False
-    while True:
-        debt = int(input('请下注：'))
-        if 0 < debt <= money:
+# money = 1000
+# while money > 0:
+#     print('你的总资产是：',money)
+#     needs_go_on = False
+#     while True:
+#         debt = int(input('请下注：'))
+#         if 0 < debt <= money:
+#             break
+#     first = randint(1,6) + randint(1,6)
+#     print('玩家摇出了%d点' % first)
+#     if first == 7 or first == 11:
+#         print('玩家赢')
+#         money += debt
+#     elif first == 2 or first == 3 or first == 12:
+#         print('庄家赢')
+#         money -= debt
+#     else:
+#         needs_go_on = True
+#         while needs_go_on:
+#             needs_go_on = False
+#             current = randint(1,6) + randint(1,6)
+#             print('玩家摇出了%d点' % current)
+#             if current == 7:
+#                 print('庄家赢')
+#                 money -= debt
+#             elif current == first:
+#                 print('玩家赢')
+#                 money += debt
+#             else:
+#                 needs_go_on = True
+# print('you are broke')
+
+"""
+生成斐波那契数列的前20个数
+"""
+# a = 0
+# b = 1
+# for _ in range(20):
+#     a,b = b,a+b
+#     print(a, end=' ')
+
+"""
+找出10000以内的完美数
+"""
+# import math
+
+# for num in range(1, 10000):
+#     result = 0
+#     for factor in range(1, int(math.sqrt(num)) + 1):
+#         if num % factor == 0:
+#             result += factor
+#             if factor > 1 and num // factor != factor:
+#                 result += num // factor
+#     if result == num:
+#         print(num)
+
+"""
+输出100以内所有的素数
+"""
+import math
+for num in range(2, 100):
+    is_prime = True
+    for factor in range(2, int(math.sqrt(num))+1):
+        if num % factor == 0:
+            is_prime = False
             break
-    first = randint(1,6) + randint(1,6)
-    print('玩家摇出了%d点' % first)
-    if first == 7 or first == 11:
-        print('玩家赢')
-        money += debt
-    elif first == 2 or first == 3 or first == 12:
-        print('庄家赢')
-        money -= debt
-    else:
-        needs_go_on = True
-        while needs_go_on:
-            needs_go_on = False
-            current = randint(1,6) + randint(1,6)
-            print('玩家摇出了%d点' % current)
-            if current == 7:
-                print('庄家赢')
-                money -= debt
-            elif current == first:
-                print('玩家赢')
-                money += debt
-            else:
-                needs_go_on = True
-print('you are broke')
-        
+    if is_prime:
+        print(num, end=' ')
