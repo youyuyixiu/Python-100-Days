cbd85815827fa64ad3ea90528951a81a3262442e
youyuyixiu <xiaoshuyi123@163.com>
2020-06-29 22:37:45 +0800

    day06-2

--- Day01-15/code/Day06/test.py
+++ Day01-15/code/Day06/test.py
@@ -17,13 +17,66 @@
 """
 重构代码
 """
-def factorial(num):
-    result = 1
-    for n in range(1, num + 1):
-        result *= n
-    return result
+# def factorial(num):
+#     result = 1
+#     for n in range(1, num + 1):
+#         result *= n
+#     return result
 
-m = int(input('m= '))
-n = int(input('n= '))
-print(factorial(m) // factorial(n) // factorial(m-n))
+# m = int(input('m= '))
+# n = int(input('n= '))
+# print(factorial(m) // factorial(n) // factorial(m-n))
+
+"""
+函数的参数
+"""
+
+# from random import randint
+
+# def roll_dice(n=2):
+#     """摇色子"""
+#     total = 0
+#     for _ in range(n):
+#         total += randint(1, 6)
+#     return total
+
+# def add(a=0, b=0, c=0):
+#     return a+b+c
+
+# print(roll_dice())
+# print(roll_dice(2))
+# print(roll_dice(3))
+
+# print(add())
+# print(add(1))
+# print(add(1,2))
+# print(add(1,2,3))
+# print(add(c=3, b=2, a=1))
+
+
+# def add(*args):
+#     total = 0
+#     for num in args:
+#         total += num
+#     return total
+
+# print(add())
+# print(add(1))
+# print(add(1,2))
+# print(add(1,2,3))
+
+"""
+练习1：实现计算求最大公约数和最小公倍数的函数。
+"""
+# def gcd(a, b):
+#     (a, b) = (b, a) if a > b else (a, b)
+#     for num in range(a, 0, -1):
+#         if a % num == 0 and b % num == 0:
+#             return num
+
+# def lcm(a, b):
+#     return a * b // gcd(a, b)
+
+# print(gcd(4,6))
+# print(lcm(4,6))
 
