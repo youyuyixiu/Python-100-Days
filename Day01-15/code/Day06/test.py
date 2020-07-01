d66b162736c97b5029e4ad289c831dae21e6302e
youyuyixiu <xiaoshuyi123@163.com>
2020-07-01 21:31:17 +0800

    day06-2

--- Day01-15/code/Day06/test.py
+++ Day01-15/code/Day06/test.py
@@ -80,3 +80,32 @@
 # print(gcd(4,6))
 # print(lcm(4,6))
 
+"""
+练习2：实现判断一个数是不是回文数的函数
+"""
+def is_palindrome(num):
+    temp = num
+    total = 0
+    while temp > 0:
+        total = total * 10 + temp % 10
+        temp //= 10
+    return total == num
+
+"""
+练习3：实现判断一个数是不是素数的函数
+"""
+def is_prime(num):
+    for factor in range(2,num):
+        if num % factor == 0:
+            return False
+    return True if num != 1 else False
+
+"""
+练习4：写一个程序判断输入的正整数是不是回文素数。
+"""
+
+if __name__ == '__main__':
+    num = int(input('请输入正整数：'))
+    if is_palindrome(num) and is_prime(num):
+        print('%d是回文素数' % num)
+
